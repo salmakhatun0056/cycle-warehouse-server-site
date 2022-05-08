44e4743f5b89b2be9a1fd2518df3ade1031cbd3a
salmakhatun12345e <ummesalma0056@gmail.com>
2022-05-08 13:34:47 +0600

    get items found

--- index.js
+++ index.js
@@ -12,12 +12,24 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.adbmu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('warehouse connected')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('warehouse').collection('items');
+
+        app.get('/items', async (req, res) => {
+            const query = {}
+            const cursor = itemsCollection.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
